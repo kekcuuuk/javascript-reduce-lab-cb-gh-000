91326132e628b74bb054bbe7b4d32f7781957b0f
Igor Morgunov <kekcuuuk@gmail.com>
2017-11-15 06:55:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-totalBatteries = batteryBatches.reduce((amount, key, value) => amount + value, 0);
+let totalBatteries = batteryBatches.reduce((amount, key, value) => amount + value, 0);
 
 const monologueLines = [
   'Who are you talking to right now?',
